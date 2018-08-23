46bca5076fdf1d5635d1fa6947e20b85afd5297f
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-08-23 16:07:24 -0500

    adding list items

---
+++ src/features/event/EventList/EventListPeople.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react';
+
+class EventListPeople extends Component {
+  render () {
+    return (
+      <List.Item>
+        <h1>Event List People</h1>
+      </List.Item>
+    )
+  }
+}
+
+export default EventListPeople;
