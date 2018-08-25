bc821daf3a5f5365d7b64527ce4c9dc7bf988503
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-08-25 14:16:43 -0500

    added in functions for form

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -3,8 +3,8 @@ import { Segment, Form, Button } from 'semantic-ui-react';
 
 class EventForm extends Component {
   render() {
+    const { handleCancel } = this.props;
     return (
-
   <Segment>
   <Form>
     <Form.Field>
@@ -30,7 +30,7 @@ class EventForm extends Component {
     <Button positive="positive" type="submit">
       Submit
     </Button>
-    <Button type="button">Cancel</Button>
+    <Button onClick={handleCancel} type="button">Cancel</Button>
   </Form>
 </Segment>
 
