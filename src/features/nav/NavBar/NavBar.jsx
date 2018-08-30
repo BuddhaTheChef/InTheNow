46481025d6f1bb9486826910644d5358c8dbe438
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-08-30 15:59:42 -0500

    adding routes and links to flow app

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import {Menu, Container, Button} from 'semantic-ui-react';
+import { NavLink, Link } from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
     return (
      <Menu inverted="inverted" fixed="top">
       <Container>
-        <Menu.Item header="header">
+        <Menu.Item as={NavLink} to='/' header="header">
           <img src="assets/logo.png" alt="logo"/>
           EventNow
         </Menu.Item>
-        <Menu.Item name="Events"/>
+        <Menu.Item as={NavLink} to='/events' name="Events"/>
+        <Menu.Item as={NavLink} to='/people' name="People"/>
         <Menu.Item>
-          <Button floated="right" positive="positive" inverted="inverted" content="Create Event"/>
+          <Button as={Link}  to='/createEvent' floated="right" positive="positive" inverted="inverted" content="Create Event"/>
         </Menu.Item>
         <Menu.Item position="right">
           <Button basic="basic" inverted="inverted" content="Login"/>
