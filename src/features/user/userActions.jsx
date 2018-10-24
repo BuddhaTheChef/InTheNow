9d68b0c747aca4abb716e9c75a84630b68e94f5a
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-10-24 14:21:43 -0500

    fixed duplicate photo issue

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import cuid from 'cuid';
 import { toastr } from 'react-redux-toastr'
 
 export const updateProfile = (user) =>
@@ -20,12 +21,13 @@ export const updateProfile = (user) =>
 
   export const uploadProfileImage = (file, fileName) =>
     async (dispatch, getState, {getFirebase, getFirestore}) => {
+      const imageName = cuid();
       const firebase = getFirebase();
       const firestore = getFirestore();
       const user = firebase.auth().currentUser;
       const path = `${user.uid}/user_images`;
       const options = {
-        name: fileName
+        name: imageName
       }
       try {
       //upload the file to firebase storage
@@ -49,7 +51,7 @@ export const updateProfile = (user) =>
         doc: user.uid,
         subcollections: [{collection: 'photos'}]
       }, {
-        name: fileName,
+        name: imageName,
         url: downloadURL
       })
       }
