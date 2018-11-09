65b503199f9c099f0fe3dfdf3001ad4308d35eba
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-09 16:29:30 -0600

    added in cloud functions

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.createActivity = functions.firestore
+.document('events/{eventId}')
+.onCreate(event => {
+  let newEvent = event.data();
+
+  console.log(newEvent);
+
+  const activity = {
+    type: 'newEvent',
+    eventDate: newEvent.date,
+    hostedBy: newEvent.hostedBy,
+    title: newEvent.title,
+    photoURL: newEvent.hostPhotoURL,
+    timestamp: admin.firestore.FieldValue.serverTimestamp(),
+    hostUid: newEvent.hostUid,
+    eventId: event.id
+  }
+  console.log(activity);
+
+  return admin.firestore().collection('activity')
+    .add(activity)
+    .then((docRef) => {
+      return console.log('Activity created with ID: ', docRef.id);
+    })
+    .catch((err) => {
+      return console.log('Error adding activity', err);
+    });
+})
